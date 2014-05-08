f0c2e222f76ca5ffb221e83966dc5568668e9f84
jcoravos <jcoravos@gmail.com>
2014-05-08 14:19:30 -0400

    %%Load Measurements Bug fixed
    Needed to be able to specify which slice to keep from the Edged data
    set. Was automatically choosing the first slice, when it should allow
    the user to choose whichever slice is appropriate.

--- LoadEdgeData.m
+++ LoadEdgeData.m
@@ -1,54 +1,63 @@
 %% Extracting Data from Edge Output
 
-frame_num = 90; %time frames in stack
-xydim = 0.15; %this is the xy dimension from the metadata, where xydim = microns/pixel. This number needs to be the same as was used in 
+frame_num = 42; %time frames in stack
+xydim = 0.13; %this is the xy dimension from the metadata, where xydim = microns/pixel. This number needs to be the same as was used in 
                 %Edge, which can be verified by looking at the .csv file in Matlab/EDGE-1.06/
     conv_fact = (1/xydim); % pixels/micron
-zkeep = '_z005'; %zslice to keep
+zkeep = '_z007'; %zslice to keep
 memchannel = '_c002'; %membrane channel
 rokchannel = '_c001'; %Rok (or other signal) channel
 
 %% File Names
 
-rokfileRoot = '/Users/Jonathan/Documents/Matlab/EDGE-1.06/DATA_GUI/CoM Testing/Myosin/Image35_082013_t';
+rokfileRoot = '/Users/jcoravos/Documents/MATLAB/EDGE-1.06/DATA_GUI/Image21_121813/Myosin/Image21_121813_t';
 %moefile = '/Users/Jonathan/MATLAB/BlebQuant/FixedMoeBlebs/PhallSqhMoe_2/PhallSqhMoe_2_z007_c003.tif'; %adjust the 'z007' here to change the slice.
-membraneRoot = '/Users/Jonathan/Documents/Matlab/EDGE-1.06/DATA_GUI/CoM Testing/Membranes/Raw/Image35_082013_t';%make this the file used for Edge membrane segmentation
-measdir = '/Users/Jonathan/Documents/Matlab/EDGE-1.06/DATA_GUI/CoM Testing/Measurements/';
+membraneRoot = '/Users/jcoravos/Documents/MATLAB/EDGE-1.06/DATA_GUI/Image21_121813/Membranes/Raw/Image21_121813_t';%make this the file used for Edge membrane segmentation
+measdir = '/Users/jcoravos/Documents/MATLAB/EDGE-1.06/DATA_GUI/Image21_121813/Measurements/';
     measCenty = 'Membranes--basic_2d--Centroid-y.mat';
     measCentx = 'Membranes--basic_2d--Centroid-x.mat';
     measVerty = 'Membranes--vertices--Vertex-y.mat';
     measVertx = 'Membranes--vertices--Vertex-x.mat';
     measArea =  'Membranes--basic_2d--Area.mat';
     measPerim = 'Membranes--basic_2d--Perimeter.mat';
+    myoInt = 'Myosin--myosin_intensity--Myosin intensity.mat';
     
     
 %% Load Measurements
     % this section extracts a single slice (specified by the y coordinate in .data(...).
     % the x coordinate is the cell index, and the y coordinate is time step.
+    
+    %NOTE: NEED TO VERIFY THAT THE second value in Centy.data(a,b,c)
+    %corresponds to the slice of the embryo you edged! 1 is the first
+    %slice, 2, is the second, etc.
+    
+    slice = 2
 Centy = load(strcat(measdir,measCenty));
-    Centy = Centy.data(:,1,:);
+    Centy = Centy.data(:,slice ,:);
     %[m n p] = size(Centy);
     %Centy = reshape(Centy,p,m);
 Centx = load(strcat(measdir,measCentx));
-    Centx = Centx.data(:,1,:);
+    Centx = Centx.data(:,slice,:);
     %[m n p] = size(Centx);
     %Centx = reshape(Centx,p,m);
 Verty = load(strcat(measdir,measVerty));
-    Verty = Verty.data(:,1,:);
+    Verty = Verty.data(:,slice,:);
     %[m n p] = size(Verty);
     %Verty = reshape(Verty,p,m);
 Vertx = load(strcat(measdir,measVertx));
-    Vertx = Vertx.data(:,1,:);
+    Vertx = Vertx.data(:,slice,:);
     %[m n p] = size(Vertx);
     %Vertx = reshape(Vertx,p,m);
 Area = load(strcat(measdir,measArea));
-     Area = Area.data(:,1,:);
+     Area = Area.data(:,slice,:);
      %[m n p] = size(Area);
      %Area = reshape(Area,p,m);
 Perim = load(strcat(measdir,measPerim));
-    Perim = Perim.data(:,1,:);
+    Perim = Perim.data(:,slice,:);
     %[m n p] = size(Perim);
     %Perim = reshape(Perim,p,m);
+RokInt = load(strcat(measdir,myoInt));
+
 %% Convert from microns to pixels
     % all the above cells are in microns. Multiply each cell by the
     % conversion factor (above) to convert to pixels
@@ -61,15 +70,10 @@ Perim = load(strcat(measdir,measPerim));
 
 %% Load Membrane and Rok movie
     %loads tif image sequences from Edge analysis into time stacks (3d) 
-    %{
-    %This section is only required if the movie is particularly large and
-    you want to improve speed by declaring membranestack and rokstack
-    before populating them.
-imread(strcat(membraneRoot,'001_z005_c002.tif'));   %declaring membranestack matrix
-[m,n] = size(ans); %this size is in pixels, not microns
-membranestack = zeros(m,n,frame_num); %x position is frame 
-rokstack = zeros(m,n,frame_num);
-    %}
+%imread(strcat(membraneRoot,'001_z005_c002.tif'));   %declaring membranestack matrix
+%[m,n] = size(ans); %this size is in pixels, not microns
+%membranestack = zeros(m,n,frame_num); %x position is frame 
+%rokstack = zeros(m,n,frame_num);
 
 for frame = 1:frame_num;
     if frame < 10
